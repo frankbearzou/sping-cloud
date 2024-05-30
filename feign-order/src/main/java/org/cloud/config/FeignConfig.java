a9e87db6b3dfbfa32b5b8c9304f25ea272b7f60d
Frank Zou <frankbearzou@gmail.com>
2024-05-30 13:55:19 -0400

    add open feign retry

--- feign-order/src/main/java/org/cloud/config/FeignConfig.java
+++ feign-order/src/main/java/org/cloud/config/FeignConfig.java
@@ -1,10 +1,16 @@
 package org.cloud.config;
 
+import feign.Retryer;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @EnableFeignClients(basePackages = {"org.cloud.commons.feign"})
 public class FeignConfig {
 
+    @Bean
+    public Retryer feignRetryer() {
+        return new Retryer.Default(100, 5000, 3);
+    }
 }
